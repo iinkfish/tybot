90f324e3b3bd29c27ca58605558dddb70ab09b7c
tychoSRC <99697508+tychoSRC@users.noreply.github.com>
2022-11-25 20:03:15 +0100

    moved setup for config such as token to main file

--- src/main.py
+++ src/main.py
@@ -1,4 +1,19 @@
 import bot
+import logging
+import json
 
 if __name__ == '__main__':
-    bot.run_discord_bot()
+
+    try:
+        f = open('./src/botConfig.json')
+    except FileNotFoundError:
+        print("There was an error opening the file. Please check path in bot.py")
+    # f = open('..\\data\\credentials.json')
+    config = json.load(f)
+
+    logging.basicConfig(filename='app.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+    logging.warning('This will get logged to a file')
+
+    TOKEN = config["botMeta"]["TOKEN"]
+
+    bot.run_discord_bot(TOKEN)
